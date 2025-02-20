770e8ee109feef4c50742e3e56b1c9422b0e60d5
ocean-ia <84613364+ocean-ia@users.noreply.github.com>
2025-02-20 11:10:43 +0700

    Delete api directory

--- api/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('GEMINI_API_KEY', 'AIzaSyABbRf2u83A-QAGLa9xp4YunF88d18xTIY');
-define('API_BASE_URL', 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent');
-?>
